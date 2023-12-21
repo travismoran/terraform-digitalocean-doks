76e93ef00ae7869d7159d435c777cfa851ae9027
Travis Moran <31007599+travismoran@users.noreply.github.com>
2023-12-21 14:13:17 -0700

    Update main.tf

--- kubernetes-config/main.tf
+++ kubernetes-config/main.tf
@@ -43,113 +43,113 @@ provider "helm" {
     )
   }
 }
-#
-#resource "kubernetes_namespace" "test" {
-#  metadata {
-#    name = "test"
-#  }
-#}
-#
-#resource "kubernetes_deployment" "test" {
-#  metadata {
-#    name = "test"
-#    namespace= kubernetes_namespace.test.metadata.0.name
-#  }
-#  spec {
-#    replicas = 2
-#    selector {
-#      match_labels = {
-#        app = "test"
-#      }
-#    }
-#    template {
-#      metadata {
-#        labels = {
-#          app  = "test"
-#        }
-#      }
-#      spec {
-#        container {
-#          image = "hashicorp/http-echo"
-#          name  = "http-echo"
-#          args  = ["-text=test"]
-#
-#          resources {
-#            limits = {
-#              memory = "512M"
-#              cpu = "1"
-#            }
-#            requests = {
-#              memory = "256M"
-#              cpu = "50m"
-#            }
-#          }
-#        }
-#      }
-#    }
-#  }
-#}
-#
-#resource "kubernetes_service" "test" {
-#  metadata {
-#    name      = "test-service"
-#    namespace = kubernetes_namespace.test.metadata.0.name
-#  }
-#  spec {
-#    selector = {
-#      app = kubernetes_deployment.test.metadata.0.name
-#    }
-#
-#    port {
-#      port = 5678
-#    }
-#  }
-#}
-#
-#resource "helm_release" "nginx_ingress" {
-#  name       = "nginx-ingress-controller"
-#  namespace  = kubernetes_namespace.test.metadata.0.name
-#
-#  repository = "https://charts.bitnami.com/bitnami"
-#  chart      = "nginx-ingress-controller"
-#
-#  set {
-#    name  = "service.type"
-#    value = "LoadBalancer"
-#  }
-#  set {
-#    name  = "service.annotations.service\\.beta\\.kubernetes\\.io/do-loadbalancer-name"
-#    value = format("%s-nginx-ingress", var.cluster_name)
-#  }
-#}
-#
-#resource "kubernetes_ingress_v1" "test_ingress" {
-#  wait_for_load_balancer = true
-#  metadata {
-#    name = "test-ingress"
-#    namespace  = kubernetes_namespace.test.metadata.0.name
-#    annotations = {
-#      "kubernetes.io/ingress.class" = "nginx"
-#      "ingress.kubernetes.io/rewrite-target" = "/"
-#    }
-#  }
-#
-#  spec {
-#    rule {
-#      http {
-#        path {
-#          backend {
-#            service {
-#              name = kubernetes_service.test.metadata.0.name
-#              port {
-#                number = 5678
-#              }
-#            }
-#          }
-#
-#          path = "/test"
-#        }
-#      }
-#    }
+
+resource "kubernetes_namespace" "test" {
+  metadata {
+    name = "test"
+  }
+}
+
+resource "kubernetes_deployment" "test" {
+  metadata {
+    name = "test"
+    namespace= kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "test"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app  = "test"
+        }
+      }
+      spec {
+        container {
+          image = "hashicorp/http-echo"
+          name  = "http-echo"
+          args  = ["-text=test"]
+
+          resources {
+            limits = {
+              memory = "512M"
+              cpu = "1"
+            }
+            requests = {
+              memory = "256M"
+              cpu = "50m"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "test" {
+  metadata {
+    name      = "test-service"
+    namespace = kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.test.metadata.0.name
+    }
+
+    port {
+      port = 5678
+    }
+  }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+  namespace  = kubernetes_namespace.test.metadata.0.name
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+  set {
+    name  = "service.annotations.service\\.beta\\.kubernetes\\.io/do-loadbalancer-name"
+    value = format("%s-nginx-ingress", var.cluster_name)
+  }
+}
+
+resource "kubernetes_ingress_v1" "test_ingress" {
+  wait_for_load_balancer = true
+  metadata {
+    name = "test-ingress"
+    namespace  = kubernetes_namespace.test.metadata.0.name
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+      "ingress.kubernetes.io/rewrite-target" = "/"
+    }
+  }
+
+  spec {
+    rule {
+      http {
+        path {
+          backend {
+            service {
+              name = kubernetes_service.test.metadata.0.name
+              port {
+                number = 5678
+              }
+            }
+          }
+
+          path = "/test"
+        }
+      }
+    }
   }
 }
